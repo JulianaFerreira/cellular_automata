f21a54ea41abfca6733566b63d0fcbc14400e0d2
Juliana <julifs.13@gmail.com>
2019-11-12 19:44:49 -0300

    add weather

--- sugarcane-ac.py
+++ sugarcane-ac.py
@@ -8,11 +8,12 @@ from PIL import Image, ImageGrab
 #test
 import numpy as np
 
-# Transition rate quality: beta - next state, gamma - state before
-beta, gamma = 0.3, 0.1
+# Transition quality
+beta = 0.3 #next state
+gamma = 0.1 #state before
 
-# Transition rate temp
-sun, rain = 0.5, 0.5
+# Transition weather
+sun, rain = 0.7, 0.2
 
 #Size
 width = 60
@@ -20,11 +21,11 @@ height = 80
 
 class Cell:
 
-    def __init__(self, time, state, phase):#put weather
+    def __init__(self, time, state, phase, weather):
         self.time = time
         self.state = state
         self.phase = phase
-        #self.weather = weather
+        self.weather = weather
 
     def gettime(self):
         return self.time
@@ -59,20 +60,22 @@ temporary = [[0 for row in range(-1, width+1)] for col in range(-1, height+1)]
 def make_frames():
     processing()
     paint_cells()
+    #paint_cells_phase()
+    #paint_cells_weather()
     root.update() #comment to make infinite
-    #root.after(1000, make_frames)
+    #root.after(1000, make_frames) #not comment to make infinite
 
 
 def put_cells():
     # state: 0 = good, 1 = medium, 2 = bad, 3 = dead
     # quality score: good: 7-10; medium: 5-7; bad: 0-5
-    # phase: 0 = bud, 1 = tillering, 2 = grow, 3 = mature, 4 = harvest, 5 = dead
-    # weather: 0 = rain, 1 = sun
+    # phase: 0 = bud, 1 = tillering, 2 = grow, 3 = mature, 4 = harvest
+    # weather: 0 = sun, 1 = rain
     # Initial quantity cell: Good - 9; Medium - 10; Bad - 1
-    aleatory_cells = [Cell(0, 1, 0), Cell(0, 0, 0), Cell(0, 0, 0), Cell(0, 1, 0), Cell(0, 0, 0),
-                      Cell(0, 1, 0), Cell(0, 0, 0), Cell(0, 1, 0), Cell(0, 1, 0), Cell(0, 2, 0),
-                      Cell(0, 0, 0), Cell(0, 1, 0), Cell(0, 0, 0), Cell(0, 0, 0), Cell(0, 0, 0),
-                      Cell(0, 1, 0), Cell(0, 1, 0), Cell(0, 1, 0), Cell(0, 0, 0), Cell(0, 1, 0)]
+    aleatory_cells = [Cell(0, 1, 0, 0), Cell(0, 0, 1, 0), Cell(0, 0, 0, 0), Cell(0, 1, 0, 0), Cell(0, 0, 0, 0),
+                      Cell(0, 1, 0, 1), Cell(0, 0, 0, 0), Cell(0, 1, 0, 0), Cell(0, 1, 0, 0), Cell(0, 2, 0, 0),
+                      Cell(0, 0, 0, 1), Cell(0, 1, 0, 1), Cell(0, 0, 0, 0), Cell(0, 0, 0, 0), Cell(0, 0, 0, 0),
+                      Cell(0, 1, 0, 0), Cell(0, 1, 0, 0), Cell(0, 1, 0, 0), Cell(0, 0, 0, 0), Cell(0, 1, 0, 1)]
 
     for y in range(-1, width+1):
         for x in range(-1, height+1):
@@ -86,82 +89,77 @@ def processing():
     cells_state_1 = 0
     cells_state_2 = 0
     cells_state_3 = 0
+    cells_sun = 0
+    cells_rain = 0
 
     for y in range(0, width):
         for x in range(0, height):
             neighbors_state0 = search_state(0, x, y)
             neighbors_state1 = search_state(1, x, y)
             neighbors_state2 = search_state(2, x, y)
+            neighbors_sun = search_state(0, x, y)
+            neighbors_rain = search_state(1, x, y)
 
             temporary[x][y] = copy.copy(previousGen[x][y])
             temporary[x][y].settime(temporary[x][y].gettime() + 15)
 
             #setphase
-            if previousGen[x][y].getphase() == 30:
-                previousGen[x][y].setphase(1)
+            if previousGen[x][y].gettime() == 30:
+                temporary[x][y].setphase(1)
 
-            elif previousGen[x][y].getphase() == 150:
-                previousGen[x][y].setphase(2)
+            elif previousGen[x][y].gettime() == 150:
+                temporary[x][y].setphase(2)
 
-            elif previousGen[x][y].getphase() == 270:
-                previousGen[x][y].setphase(3)
+            elif previousGen[x][y].gettime() == 270:
+                temporary[x][y].setphase(3)
 
-            elif previousGen[x][y].getphase() == 480:
-                previousGen[x][y].setphase(4)
-
+            elif previousGen[x][y].gettime() == 480:
+                temporary[x][y].setphase(4)
 
 
             #setweather
+            if previousGen[x][y].getweather() == 0:
+                cells_sun += 1
+                temporary[x][y].setweather(getNewWeather(previousGen[x][y].getweather(), neighbors_rain))
+            elif previousGen[x][y].getweather() == 1:
+                cells_rain += 1
+                temporary[x][y].setweather(getNewWeather(previousGen[x][y].getweather(), neighbors_sun))
 
-
-
-
-
-
-            #statistic
-            #temporary[x][y].setstate(getNewState2D(previousGen[x][y].getstate(), infected_neighbors_state1, infected_neighbors_state2))
 
             #setstate
             if previousGen[x][y].getstate() == 0:
                 cells_state_0 += 1
                 temporary[x][y].setstate(
-                    getNewState2D(previousGen[x][y].getstate(), neighbors_state1, neighbors_state2))
-                if neighbors_state1 > 4: #and previousGen[x][y].getphase() == 0:
-                    temporary[x][y].setstate(0) #1
-                elif neighbors_state2 > 5:
-                    temporary[x][y].setstate(0)#2
+                    getNewQuality(previousGen[x][y].getstate(), neighbors_state1, neighbors_state2))
 
             elif previousGen[x][y].getstate() == 1:
                 cells_state_1 += 1
                 temporary[x][y].setstate(
-                    getNewState2D(previousGen[x][y].getstate(), neighbors_state0, neighbors_state2))
-                if neighbors_state0 > 4:  # or neighbors_state2 > 3:and previousGen[x][y].getphase() == 0:
-                    temporary[x][y].setstate(1)#0
-                elif neighbors_state2 > 5:
-                    temporary[x][y].setstate(1)#2
+                    getNewQuality(previousGen[x][y].getstate(), neighbors_state0, neighbors_state2))
 
             elif previousGen[x][y].getstate() == 2:
                 cells_state_2 += 1
                 temporary[x][y].setstate(
-                    getNewState2D(previousGen[x][y].getstate(), neighbors_state1, neighbors_state2))
-                if neighbors_state0 > 6:  # and previousGen[x][y].getphase() == 0:
-                    temporary[x][y].setstate(2)#0
-                elif neighbors_state1 > 5:
-                    temporary[x][y].setstate(2)#1
-                elif neighbors_state2 > 2:
-                    cells_state_3 += 1
-                    temporary[x][y].setstate(2)#3
+                    getNewQuality(previousGen[x][y].getstate(), neighbors_state1, neighbors_state2))
+
+            elif previousGen[x][y].getstate() == 3:
+                cells_state_3 += 1
 
 
     archive = open("ac.txt", "a")
     archive2 = open("ac.txt", "r")
     content = archive2.readlines()
-    archive.write("dias: %d" % temporary[0][0].gettime() + "\n")
+    archive.write("dia: %d" % temporary[0][0].gettime() + "\n")
+    archive.write("----------------------------" + "\n")
     archive.write("cells good: %d" % cells_state_0 + "\n")
     archive.write("cells medium: %d" % cells_state_1 + "\n")
     archive.write("cells bad: %d" % cells_state_2 + "\n")
     archive.write("cells dead: %d" % cells_state_3 + "\n")
     archive.write("----------------------------" + "\n")
+    archive.write("weather sun: %d" % cells_sun + "\n")
+    archive.write("weather rain: %d" % cells_rain + "\n")
+    archive.write("----------XXXXX------------" + "\n")
+
 
     for y in range(0, width):
         for x in range(0, height):
@@ -197,47 +195,86 @@ def search_state(state, a, b):
 
     return count
 
+def search_weather(state, a, b):
+    count = 0
+
+    if previousGen[a - 1][b + 1].getweather() == state or previousGen[a][b + 1].getweather() == state:
+        count += 1
+
+    if previousGen[a][b + 1].getweather() == state:
+        count += 1
+
+    if previousGen[a + 1][b + 1].getweather() == state:
+        count += 1
+
+    if previousGen[a - 1][b].getweather() == state:
+        count += 1
+
+    if previousGen[a + 1][b].getweather() == state:
+        count += 1
+
+    if previousGen[a - 1][b - 1].getweather() == state:
+        count += 1
+
+    if previousGen[a][b - 1].getweather() == state:
+        count += 1
+
+    if previousGen[a + 1][b - 1].getweather() == state:
+        count += 1
+
+    return count
+
 
 def getRandomNumber(distribution):
     if distribution == 0:
         returningRandomNumber = np.random.uniform() # UNIFORM
     elif distribution == 1:
         returningRandomNumber = np.random.normal(.5, .1) # NORMAL
-    elif distribution == 2:
-        returningRandomNumber = (np.random.binomial(20, .5, 100) % 10) * 0.1 # BINOMIAL
-    elif distribution == 3:
-        returningRandomNumber = np.random.poisson(2) * .1 # POISSON
     return returningRandomNumber
 
-def getNewState2D(selfCharacter,neighbors_state1,neighbors_state2):
+def getNewQuality(selfCharacter, neighbors_state1, neighbors_state2):
     newState = selfCharacter
+    chance = getRandomNumber(0)
 
     if selfCharacter == 0: # If G
-        if neighbors_state1 > 2 or neighbors_state2 > 2: #M or B close
-            chance = getRandomNumber(0)
+        if neighbors_state1 > 5 or neighbors_state2 > 6: #M or B close
             if chance < beta and chance > 0:
                 newState = 1
 
     elif selfCharacter == 1: # If M
-        chance = getRandomNumber(0)
-        if neighbors_state1 > 2:  # G close
+        if neighbors_state1 > 4:  # G close
             if chance < gamma and chance > 0:
                 newState = 0
-        elif neighbors_state2 > 2:  # B close
+        elif neighbors_state2 > 5:  # B close
             if chance < beta and chance > 0:
                 newState = 2
 
     elif selfCharacter == 2: # If B
-        chance = getRandomNumber(0)
-
-        if neighbors_state1 > 2:  # G close
+        if neighbors_state1 > 4:  # G close
             if chance < gamma and chance > 0:
                 newState = 1
-        elif neighbors_state2 > 2:  # B close
+        elif neighbors_state2 > 5:  # B close
             if chance < beta and chance > 0:
                 newState = 3
 
     return newState
+
+def getNewWeather(selfCharacter, neighbors_state):
+    newState = selfCharacter
+    chance = getRandomNumber(0)
+
+    if selfCharacter == 0: # If Sun
+        if neighbors_state > 4:
+            if chance < rain and chance > 0:
+                newState = 1
+
+    elif selfCharacter == 1: # If Rain
+        if neighbors_state > 2:
+            if chance < sun and chance > 0:
+                newState = 0
+
+    return newState
+
 
 def paint_cells():
     for y in range(width):
@@ -250,6 +287,28 @@ def paint_cells():
                 canvas.itemconfig(cell[x][y], fill="red")
             elif previousGen[x][y].getstate() == 3:
                 canvas.itemconfig(cell[x][y], fill="black")
+
+def paint_cells_weather():
+    for y in range(width):
+        for x in range(height):
+            if previousGen[x][y].getweather() == 0:
+                canvas.itemconfig(cell[x][y], fill="yellow")
+            elif previousGen[x][y].getweather() == 1:
+                canvas.itemconfig(cell[x][y], fill="blue")
+
+def paint_cells_phase():
+    for y in range(width):
+        for x in range(height):
+            if previousGen[x][y].getphase() == 0:
+                canvas.itemconfig(cell[x][y], fill="#f9eebd")
+            elif previousGen[x][y].getphase() == 1:
+                canvas.itemconfig(cell[x][y], fill="#e7f9bd")
+            elif previousGen[x][y].getphase() == 2:
+                canvas.itemconfig(cell[x][y], fill="#c2f7a8")
+            elif previousGen[x][y].getphase() == 3:
+                canvas.itemconfig(cell[x][y], fill="#56fc2d")
+            elif previousGen[x][y].getphase() == 4:
+                canvas.itemconfig(cell[x][y], fill="#f9ccbd")
 
 
 root = Tk()
